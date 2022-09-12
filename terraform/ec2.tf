fffcbed360c7eebd6fc8e00635319f70afaf077b
Cesar Augusto <cesar.bruschetta@olxbr.com>
2022-09-12 14:56:18 -0300

    Update terraform to ec2

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -7,18 +7,26 @@ data "aws_ami" "ubuntu" {
   }
 }
 
+resource "tls_private_key" "this" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
 resource "aws_key_pair" "this" {
   key_name   = "${local.prefix}-key"
-  public_key = var.aws_public_key
+  public_key = tls_private_key.this.public_key_openssh
 }
 
 resource "aws_instance" "this" {
+  depends_on = [
+    aws_efs_mount_target.efs-mt-site
+  ]
+
   ami                         = data.aws_ami.ubuntu.id
   instance_type               = var.aws_instance_type
-  count                       = 2
+  count                       = 1
   key_name                    = aws_key_pair.this.key_name
   associate_public_ip_address = true
-  user_data                   = file("./scripts/nginx.sh")
 
   vpc_security_group_ids = [aws_security_group.sg-web.id]
   subnet_id              = aws_subnet.this.id
@@ -29,26 +37,47 @@ resource "aws_instance" "this" {
     }
   )
 
-  depends_on = [
-    aws_ebs_volume.this
-  ]
+  connection {
+    type     = "ssh"
+    user     = "ubuntu"
+    private_key = tls_private_key.this.private_key_pem
+    host     = self.public_ip
+  }
+
+  # MOUNT EFS VOLUME
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get update",
+      "sudo apt-get install -y curl wget nfs-common",
+      "sudo mkdir -p /mnt/website",
+      "sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport ${aws_efs_mount_target.efs-mt-site.dns_name}:/ /mnt/website",
+      "sudo curl -fsSL https://get.docker.com | bash",
+      "sudo docker run --name ${local.prefix}-website-nginx -v /mnt/website:/usr/share/nginx/html:ro -d nginx",
+    ]
+  }
+  
+  # Copy files to EFS
+  provisioner "file" {
+    source      = "../website"
+    destination = "/mnt/"
+  }
 
 }
 
-resource "aws_ebs_volume" "this" {
-  availability_zone = "${var.aws_region}a"
-  size              = 10
-
+resource "aws_efs_file_system" "efs-site" {
+  creation_token = "site"
+  performance_mode = "generalPurpose"
+  throughput_mode = "bursting"
   tags = merge(
     local.common_tags,
     {
-      Name = "${local.prefix}-volume-site"
+      Name = "${local.prefix}-source-site"
     }
   )
 }
 
-resource "aws_volume_attachment" "ec2_1" {
-  device_name = "/dev/sdh"
-  volume_id   = aws_ebs_volume.this.id
-  instance_id = aws_instance.this.0.id
-}
+resource "aws_efs_mount_target" "efs-mt-site" {
+   file_system_id  = "${aws_efs_file_system.efs-site.id}"
+   subnet_id = "${aws_subnet.this.id}"
+   security_groups = ["${aws_security_group.ingress-efs-site.id}"]
+ }
